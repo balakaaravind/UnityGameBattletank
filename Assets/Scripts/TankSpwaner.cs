117a66764a955f67c27e8cbbacc96784dc6435a4
balakaaravind <125255565+balakaaravind@users.noreply.github.com>
2023-07-25 14:14:50 +0530

    Added Tank Spawner Script
    Spawned tank in the game using script

--- Assets/Scripts/TankSpwaner.cs
+++ Assets/Scripts/TankSpwaner.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class TankSpwaner : MonoBehaviour
 {
+    public GameObject tankPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(tankPrefab, transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
